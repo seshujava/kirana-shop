7711d5fa8f53f707da372444a42bd9ed7f446d20
seshu Chowdary <seshu Chowdary@Seshu.Dlink>
2020-07-12 19:16:40 +0300

    Initial commit. Create spring boot project and configure the application properties.

---
+++ chitturikiranashop/src/main/java/in/seshujava/chitturikiranashop/ChitturikiranashopApplication.java
@@ -0,0 +1,12 @@
+package in.seshujava.chitturikiranashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitturikiranashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitturikiranashopApplication.class, args);
+	}
+}
